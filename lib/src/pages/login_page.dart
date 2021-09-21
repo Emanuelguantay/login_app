69eeb3d5e3e6f2bceed9183f7163dc0e817ed54d
Emanuelguantay <emanuel.guantay92@gmail.com>
2021-09-21 18:29:19 -0300

    :+1:  Twitter

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:twitter_login/twitter_login.dart';
 //import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:firebase_core/firebase_core.dart';
 
@@ -45,13 +46,11 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 onPressed: () async {
                   await onPressGoogleLogin();
-                  print("Prueba");
+                  print("Google");
                   //onPressGoogleLogin();
                 },
               ),
-              
               SizedBox(height: 20),
-        
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.blue),
                 child: Text(
@@ -64,7 +63,24 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 onPressed: () async {
                   await onPressGoogleLogin();
-                  print("Prueba");
+                  print("Facebook");
+                  //onPressGoogleLogin();
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.lightBlue),
+                child: Text(
+                  "Twitter",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                  ),
+                ),
+                onPressed: () async {
+                  await signInWithTwitter();
+                  print("Twitter");
                   //onPressGoogleLogin();
                 },
               ),
@@ -80,13 +96,59 @@ class _LoginPageState extends State<LoginPage> {
       GoogleSignInAccount? result = await _googleSignIn!.signIn();
 
       if (result != null) {
-        print (result.id);
+        print(result.id);
         String firstName = "";
         String lastName = "";
-        
       }
     } catch (error) {
       print(error);
     }
+  }
+
+  onPressTwitterLogin() async {
+    try {
+      GoogleSignInAccount? result = await _googleSignIn!.signIn();
+
+      if (result != null) {
+        print(result.id);
+        String firstName = "";
+        String lastName = "";
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<bool?> signInWithTwitter() async {
+    try {
+      final twitterLogin = TwitterLogin(
+        apiKey: "IOCSQXQaLlHdlDTp1aruqj89d",
+        apiSecretKey: "YEgKy2hiJwlWIdsSvJgMbirUeHOm1MHWF36JNUO3AoWg2tTOAQ",
+        redirectURI: "edesa-app://",
+      );
+      final authResult = await twitterLogin.login();
+
+      print(authResult);
+    } catch (error) {
+      print(error);
+    }
+
+    // switch (authResult.status) {
+    //   case TwitterLoginStatus.loggedIn:
+    //     final AuthCredential twitterAuthCredential =
+    //         TwitterAuthProvider.credential(
+    //             accessToken: authResult.authToken!,
+    //             secret: authResult.authTokenSecret!);
+
+    //     final userCredential =
+    //         await _auth.signInWithCredential(twitterAuthCredential);
+    //     return Resource(status: Status.Success);
+    //   case TwitterLoginStatus.cancelledByUser:
+    //        return Resource(status: Status.Success);
+    //   case TwitterLoginStatus.error:
+    //      return Resource(status: Status.Error);
+    //   default:
+    //     return null;
+    //  }
   }
 }
